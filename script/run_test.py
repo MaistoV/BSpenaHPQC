778b261181d4482c047bbd1b7cf28978f203ebdf
bspena <119626673+bspena@users.noreply.github.com>
2024-02-17 11:25:30 +0100

    Update

--- script/run_test.py
+++ script/run_test.py
@@ -3,6 +3,19 @@ import xml.etree.ElementTree as ET
 import os
 import subprocess
 
+# xml and csv files path
+path_to_hdfs_site = '/home/spena/Desktop/hdfs-site.xml'
+path_to_mapred_site = '$HADOOP_HOME/etc/hadoop/mapred-site.xml'
+path_to_yarn_site = '$HADOOP_HOME/etc/hadoop/yarn-site.xml'
+path_to_test_list = '/home/spena/Desktop/test_list.csv'
+#path_to_test_result = 
+
+# Tuples with the parameters (same order as of test_list file)
+hdfs_p = ('dfs.namenode.handler.count','dfs.datanode.handler.count')
+mapred_p = ('mapreduce.job.reduces','mapreduce.reduce.cpu.vcores')
+yarn_p = ('yarn.scheduler.minimum-allocation-vcores','yarn.scheduler.maximum-allocation-vcores','yarn.resourcemanager.scheduler.class')
+
+
 # Function to configure di parameters, it receives :
 # 1. the xml file root (configuration)
 # 2. the name of the parameter
@@ -36,33 +49,33 @@ def update_xml(file,row,tuple):
 
     tree.write(file, encoding="utf-8", xml_declaration=True)        # Write on xml file
 
+# Function for the cluster configuration, it takes:
+#1. the path to the hdfs-site.xml
+#2. the tuple with hdfs parameters
+#3. the path to the mapred-site.xml
+#4. the tuple with mapred parameters
+#5. the path to the yarn-site.xml
+#6. the tuple with yarn parameters
+#7. the dataframe row
+def config_cluster(path_to_hdfs_site,hdfs_p,path_to_mapred_site,mapred_p,path_to_yarn_site,yarn_p,row):
+    update_xml(path_to_hdfs_site,row,hdfs_p)
+    #update_xml(path_to_mapred_site,row,mapred_p)
+    #update_xml(path_to_yarn_site,row,yarn_p)
+
 
 if __name__=='__main__':
     
-    # xml and csv files path
-    path_to_hdfs_site = 
-    #path_to_mapred_site =
-    #path_to_yarn_site =
-    path_to_test_list = 
-    #path_to_test_result = 
-
-    # Tuples with the parameters (same order as of test_list file)
-    hdfs_p = ('dfs.namenode.handler.count','dfs.datanode.handler.count')
-    mapred_p = ('mapreduce.job.reduces','mapreduce.reduce.cpu.vcores')
-    yarn_p = ('yarn.scheduler.minimum-allocation-vcores','yarn.scheduler.maximum-allocation-vcores','yarn.resourcemanager.scheduler.class')
-
-
     #1 Read test_list.csv file and saves the parameters in a dataframe
+    print("Step 1 : Read test_list.csv \n")
     dataframe = pandas.read_csv(path_to_test_list)                  
     dataframe.index = ['test1','test2']
 
     # For each dataframe row are executed the the steps from 2 to 6
     for i,row in dataframe.iterrows():
         
-        #2 Configures the Hadoop clusters by setting **-site.xml* files
-        update_xml(path_to_hdfs_site,row,hdfs_p)
-        #update_xml(path_to_mapred_site,row,mapred_p)
-        #update_xml(path_to_yarn_site,row,yarn_p)
+        #2. Cluster configuration by setting **-site.xml* files
+        print("Step 2 : Cluster Configuration \n")
+        config_cluster(path_to_hdfs_site,hdfs_p,path_to_mapred_site,mapred_p,path_to_yarn_site,yarn_p,row)
         
         #3 Start the cluster in pseudo-distributed mode
             # Stop hdfs and yarn deamons (va fatto per ogni test)
