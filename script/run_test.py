eb47e286de68ad0fb7364ad8177e6ae7e4963025
bspena <119626673+bspena@users.noreply.github.com>
2024-02-20 23:43:32 +0100

    Update

--- script/run_test.py
+++ script/run_test.py
@@ -1,10 +1,11 @@
 import pandas
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET                                  # Module for parsing and creating XML data.
 import os
-import requests
+import requests                                                     # Module to execute http requests
 import subprocess
 import time
-
+import csv
+import linecache                                                    # Module to axtract and access specific lines in python
 
 # Function to add tags to the xml file, it receives :
     # 1. the xml file root (configuration)
@@ -19,7 +20,6 @@ def add_tags(root,n,v):
     root.append(property)                                           # Add the new elements to the root element
     property.append(name)
     property.append(value)
-
 
 
 # Function to update the xml file, it receives :
@@ -53,10 +53,10 @@ def update_xml(file,row,tuple,special_parameters):
     #6. the tuple with yarn parameters
     #7. the dataframe row
     #8. the special parameters need for the cluster configuration
-def config_cluster(path_to_hdfs_site,hdfs_t,path_to_mapred_site,mapred_t,path_to_yarn_site,yarn_t,row,special_parameters):
-    update_xml(path_to_hdfs_site,row,hdfs_t,special_parameters)                 # Configure hdfs-site.xml
-    update_xml(path_to_mapred_site,row,mapred_t,special_parameters)             # Configure mapred-site.xml
-    update_xml(path_to_yarn_site,row,yarn_t,special_parameters)                 # Configure yarn-site.xml
+def config_cluster(path_hdfs_site,hdfs_t,path_mapred_site,mapred_t,path_yarn_site,yarn_t,row,special_parameters):
+    update_xml(path_hdfs_site,row,hdfs_t,special_parameters)                 # Configure hdfs-site.xml
+    #update_xml(path_mapred_site,row,mapred_t,special_parameters)             # Configure mapred-site.xml
+    #update_xml(path_yarn_site,row,yarn_t,special_parameters)                 # Configure yarn-site.xml
 
 
 # Function to start the cluster
@@ -65,9 +65,10 @@ def start_cluster():
     os.system('$HADOOP_HOME/sbin/stop-yarn.sh')
     os.system('$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_HOME/etc/hadoop stop historyserver')
     time.sleep(2)
-    os.system('$HADOOP_HOME/bin/hdfs namenode -format')             # Format the filesystem
+    #os.system('$HADOOP_HOME/bin/hdfs namenode -format')             # Format the filesystem
     time.sleep(2)
     os.system('$HADOOP_HOME/sbin/start-dfs.sh')                     # Start hdfs and yarn deamons 
+    os.system('$HADOOP_HOME/bin/hdfs dfsadmin -safemode leave')
     os.system('$HADOOP_HOME/sbin/start-yarn.sh')
     os.system('$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_HOME/etc/hadoop start historyserver')
     os.system('$HADOOP_HOME/bin/hdfs dfs -mkdir /user')             # Make the HDFS directories required to execute MapReduce jobs
@@ -84,6 +85,7 @@ def start_dfsio(row,dfsio_t):
     for t in dfsio_t:
         s = s + ' -' + t.split('.')[1] + ' ' + str(row[t])  
 
+    print(s)
     os.system(s)
 
     # Start online test like linuxperf (not implemented yet)
@@ -91,24 +93,70 @@ def start_dfsio(row,dfsio_t):
     # Effettuare fork e poi join
 
 
+# Function to start test via command line
+def test_via_command_line():
+    # Find jobID
+    job_id_sub = subprocess.run('$HADOOP_HOME/bin/mapred job -list all | grep "job_"',shell = True ,capture_output=True)
+    job_id = job_id_sub.stdout.decode().split('\t')[0]
 
-def start_offline_test():
-    print("\n")
+    #1: Find the number of map tasks
+    map_number_sub = subprocess.run('$HADOOP_HOME/bin/mapred job -status ' + job_id + ' | grep "Number of maps"',shell = True ,capture_output=True)
+    map_number = int(map_number_sub.stdout.decode().split(':')[1])
+
+    #2: the CPU time spent for map tasks and reduce tasks
+    cpu_time_sub = subprocess.run('$HADOOP_HOME/bin/mapred job -history ' + job_id + ' | grep "CPU time spent"',shell = True ,capture_output=True)
+    cpu_time_map = int(cpu_time_sub.stdout.decode().replace(',','').split('|')[3])
+    cpu_time_red = int(cpu_time_sub.stdout.decode().replace(',','').split('|')[4])
+    cpu_time_tot = int(cpu_time_sub.stdout.decode().replace(',','').split('|')[5])
+
+    return map_number,cpu_time_map,cpu_time_red,cpu_time_tot
+
+
+# Function to read response variables from TestDFSIO logs, it takes:
+    #1. index of the test, to increase the row
+    #2. path to the log file
+def test_dfsio_logs(index,file):
+    throughput_line =  5 + (9 * index)                                      # Position of the lines
+    avarege_io_line =  6 + (9 * index)
+    throughput_value = float(linecache.getline(file, throughput_line).split(':')[1])              # Get a specific line
+    avarege_io_value = float(linecache.getline(file, avarege_io_line).split(':')[1])
+    return throughput_value,avarege_io_value
+
+
+# Function to start offline test and to save response variables, it takes:
+    #1. index of the test, to increase the row
+    #2. path to the log file
+    #3. path to the result file
+def start_offline_test(index,path_test_dfsio_logs,path_test_result):
+
+    # Test via command line
+    map_number,cpu_time_map,cpu_time_red,cpu_time_tot = test_via_command_line()
+    
+    # Results TestDFSIO via logs
+    throughput_value,avarege_io_value = test_dfsio_logs(index,path_test_dfsio_logs)
+
+    # Test via REST API
+
+    # Saves value on test_result.csv
+    with open(path_test_result,'a') as file:          
+        writer=csv.writer(file)
+        writer.writerow([map_number,cpu_time_map,cpu_time_red,cpu_time_tot,throughput_value,avarege_io_value]) 
 
 
 if __name__=='__main__':
 
-    # xml and csv files path
-    path_to_hdfs_site = '/home/spena/hadoop/hadoop-dist/target/hadoop-3.3.5/etc/hadoop/hdfs-site.xml'
-    path_to_mapred_site = '/home/spena/hadoop/hadoop-dist/target/hadoop-3.3.5/etc/hadoop/mapred-site.xml'
-    path_to_yarn_site = '/home/spena/hadoop/hadoop-dist/target/hadoop-3.3.5/etc/hadoop/yarn-site.xml'
-    path_to_test_list = '/home/spena/Desktop/test_list.csv'
-    #path_to_test_result = 
+    # Files path
+    path_hdfs_site = '/home/spena/hadoop/hadoop-dist/target/hadoop-3.3.5/etc/hadoop/hdfs-site.xml'
+    path_mapred_site = '/home/spena/hadoop/hadoop-dist/target/hadoop-3.3.5/etc/hadoop/mapred-site.xml'
+    path_yarn_site = '/home/spena/hadoop/hadoop-dist/target/hadoop-3.3.5/etc/hadoop/yarn-site.xml'
+    path_test_list = 'test_list.csv'
+    path_test_result = 'test_result.csv'
+    path_test_dfsio_logs = 'TestDFSIO_results.log'
 
     # Tuples with the parameters (same order as of test_list file)
     hdfs_t = ('dfs.namenode.handler.count','dfs.datanode.handler.count')
     mapred_t = ('mapreduce.job.reduces','mapreduce.reduce.cpu.vcores')
-    yarn_t = ('yarn.scheduler.minimum-allocation-vcores','yarn.scheduler.maximum-allocation-vcores','yarn.resourcemanager.scheduler.class')
+    yarn_t = ('yarn.scheduler.minimum-allocation-vcores',)
     dfsio_t = ('dfsio.nrFiles','dfsio.fileSize')
 
     # String array with the special parameters needed for the cluster configuration in psuedo-distributed mode
@@ -117,15 +165,19 @@ if __name__=='__main__':
 
     #Step 1: Read test_list.csv file and saves the parameters in a dataframe
     print("Step 1: Read test_list.csv \n")
-    dataframe = pandas.read_csv(path_to_test_list)                  
+    dataframe = pandas.read_csv(path_test_list)                  
     dataframe.index = ['test1']
 
+    #Step 1.1 : Definie colums names of test_result.csv
+    with open(path_test_result,'a') as file:          
+        writer=csv.writer(file)
+        writer.writerow(['maps.number','cpu.time.map.task','cpu.time.reduce.tasks','cpu.time.tot','throughput_value','avarege_io_value'])
 
     for i,row in dataframe.iterrows():
         
         #Step 2: Cluster configuration by setting **-site.xml* files
         print("Step 2: Cluster Configuration \n")
-        config_cluster(path_to_hdfs_site,hdfs_t,path_to_mapred_site,mapred_t,path_to_yarn_site,yarn_t,row,special_parameters)
+        config_cluster(path_hdfs_site,hdfs_t,path_mapred_site,mapred_t,path_yarn_site,yarn_t,row,special_parameters)
         
         #Step 3: Start the cluster in Pseudo-Distributed Mode
         print("Step 3: Start the cluster in pseudo-distributed mode")
@@ -136,9 +188,9 @@ if __name__=='__main__':
         print("Step 4: Start the TestDFSIO")
         start_dfsio(row,dfsio_t)
                                 
-        #Step 5: Start the offline test and saves the results in *test_result.csv* file
+        #Step 5: Start the offline test and save save response variables in *test_result.csv* file
         print("Step 5: Start the Offline Test")
-        start_offline_test()
+        start_offline_test(int(i.split('test')[1]),path_test_dfsio_logs,path_test_result)
         print("\n")
 
         #Step 6: Clean up test results 
