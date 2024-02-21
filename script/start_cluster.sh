b50618cc95aa740778a95627a96908c42ef48dfe
bspena <119626673+bspena@users.noreply.github.com>
2024-02-22 00:32:35 +0100

    Update

--- script/start_cluster.sh
+++ script/start_cluster.sh
@@ -3,8 +3,8 @@
 # Stop HDFS deamons,YARN deamons and JobHistoryServer
 $HADOOP_HOME/sbin/stop-dfs.sh                      
 $HADOOP_HOME/sbin/stop-yarn.sh
-$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_HOME/etc/hadoop stop historyserver
-sleep 5
+$HADOOP_HOME/bin/mapred --daemon stop historyserver
+sleep 4
 
 # Format the filesystem
 #$HADOOP_HOME/bin/hdfs namenode -format             
@@ -12,9 +12,9 @@ sleep 5
 
 # Start HDFS deamons,YARN deamons and JobHistoryServer
 $HADOOP_HOME/sbin/start-dfs.sh                     
-$HADOOP_HOME/bin/hdfs dfsadmin -safemode leave              # Forcefully let the namenode leave safemode
+$HADOOP_HOME/bin/hdfs dfsadmin -safemode leave                  # Forcefully let the namenode leave safemode
 $HADOOP_HOME/sbin/start-yarn.sh
-$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_HOME/etc/hadoop start historyserver
+$HADOOP_HOME/bin/mapred --daemon start historyserver
 sleep 2
 
 # Make the HDFS directories required to execute MapReduce jobs
